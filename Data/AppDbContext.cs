32b05a4697870d6ea04fd6af6455a6e9eda74f12
Abdallah Shadad <abdallhshadad8@gmail.com>
2026-04-08 16:38:46 +0200

    add models and DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // 1. Constructor: Passes configuration (like the connection string) to the base class
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // 2. Optional: Configure relationships and constraints using Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+
+
+        // 3. Tables: These properties represent the tables in your database
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
