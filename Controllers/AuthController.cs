58b3bf910daccf53fc246e8e8dbd759b6a9e0df0
Abdallah Shadad <abdallhshadad8@gmail.com>
2026-04-08 17:55:20 +0200

    Auth Controller implementation

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,47 @@
-﻿namespace TodoListApi.Controllers
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using TodoListApi.DTOs;
+using TodoListApi.Services;
+
+namespace TodoListApi.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            try
+            {
+                var result = await _authService.Register(dto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            try
+            {
+                var result = _authService.Login(dto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
     }
 }
